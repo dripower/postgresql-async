0ed95a8cf3e14619fafca48f2bc5e12ff7d77966
jilen <jilen.zhang@gmail.com>
2017-06-28 12:47:42 +0800

    release new version

--- project/Build.scala
+++ project/Build.scala
@@ -50,7 +50,7 @@ object ProjectBuild extends Build {
 object Configuration {
 
   val nettyVersion = "4.0.44.Final"
-  val commonVersion = "0.2.2012-SNAPSHOT"
+  val commonVersion = "0.2.2012"
   val projectScalaVersion = "2.11.11"
   val specs2Version = "2.4.17"
 
