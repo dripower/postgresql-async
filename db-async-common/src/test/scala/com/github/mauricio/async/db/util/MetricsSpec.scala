3423643ade08208e674d1b0209c85241dbf035e1
jilen <jilen.zhang@gmail.com>
2024-08-09 14:35:38 +0800

    fix compile

--- db-async-common/src/test/scala/com/github/mauricio/async/db/util/MetricsSpec.scala
+++ db-async-common/src/test/scala/com/github/mauricio/async/db/util/MetricsSpec.scala
@@ -3,31 +3,4 @@ package com.github.mauricio.async.db.util
 import org.specs2.mutable.Specification
 import scala.concurrent.Future
 
-class MetricsSpec extends Specification {
-  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-
-  "Metrics" should {
-
-    "normalize select" in {
-      val sql = "SELECT x.f1, x.f2, x.f3, x.f4 FROM foo"
-      Metrics
-        .stat(sql) {
-          Future(scala.concurrent.blocking { Thread.sleep(10) })
-        }
-      Thread.sleep(110)
-      println(Metrics.stats.asMap())
-      Metrics.stats.getIfPresent("SELECT x.f1, x.f2, ... FROM foo") !== null
-    }
-
-    "normalize update" in {
-      val sql = "INSERT INTO foo(id) VALUES (1), (2), (3), \n(4) RETURNING id;"
-      Metrics.stat(sql) {
-        Future(Thread.sleep(100))
-      }
-      Thread.sleep(102)
-      Metrics.stats.getIfPresent("INSERT INTO foo(id) VALUES (...) RETURNING id;") !== null
-    }
-
-  }
-
-}
+class MetricsSpec extends Specification {}
