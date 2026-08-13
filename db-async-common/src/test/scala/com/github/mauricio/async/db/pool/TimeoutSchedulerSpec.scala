42c323038b8b3d2350a69008c27011dfd439899d
jilen <jilen.zhang@gmail.com>
2026-08-13 15:05:48 +0800

    TimeoutSchedulerSpec 改用 org.specs2.mutable.Specification（specs2-junit 已移除）

--- db-async-common/src/test/scala/com/github/mauricio/async/db/pool/TimeoutSchedulerSpec.scala
+++ db-async-common/src/test/scala/com/github/mauricio/async/db/pool/TimeoutSchedulerSpec.scala
@@ -17,7 +17,7 @@ package com.github.mauricio.async.db.pool
 
 import java.util.concurrent.{ScheduledFuture, TimeoutException}
 import com.github.mauricio.async.db.util.{ByteBufferUtils, ExecutorServiceUtils}
-import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.mutable.Specification
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Future, Promise}
@@ -25,7 +25,7 @@ import scala.concurrent.{Future, Promise}
 /**
  * Tests for TimeoutScheduler
  */
-class TimeoutSchedulerSpec extends SpecificationWithJUnit {
+class TimeoutSchedulerSpec extends Specification {
 
   val TIMEOUT_DID_NOT_PASS = "timeout did not pass"
 
